a546ab5fbbe9cac6ea729c518a7f8ad78042ef7b
Felix Boucher <fboucher9@gmail.com>
2019-11-20 07:28:26 -0500

    include protection for cv_manager.h

--- cv_manager.h
+++ cv_manager.h
@@ -1,6 +1,12 @@
 /* See LICENSE for license details */
 
+#ifndef cv_manager_h_
+#define cv_manager_h_
+
 char cv_manager_load(void);
 
 void cv_manager_unload(void);
 
+#endif /* #ifndef cv_manager_h_ */
+
+/* end-of-file: cv_manager.h */
