983c80f5113626beaf14748c3a78df2d25cfea25
Felix Boucher <fboucher9@gmail.com>
2019-11-23 08:00:13 -0500

    use runtime wrapper for printf

--- cv_debug.c
+++ cv_debug.c
@@ -6,15 +6,13 @@
 
 #include <cv_unused.h>
 
-#if defined cv_have_libc_
-#include <stdio.h>
-#endif /* #if defined cv_have_libc_ */
+#include <cv_runtime.h>
 
 void cv_debug_msg(
     char const * p_msg0)
 {
 #if defined cv_have_libc_
-    printf("*** ouch! %s ***\n",
+    cv_runtime_printf("*** ouch! %s ***\n",
         p_msg0);
 #else /* #if defined cv_have_libc_ */
     cv_unused_(p_msg0);
