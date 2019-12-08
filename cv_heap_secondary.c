76630a3348e4808c6a72aa9c630c8340c6d789bc
Felix Boucher <fboucher9@gmail.com>
2019-12-08 09:33:28 -0500

    debug code for heap_secondary

--- cv_heap_secondary.c
+++ cv_heap_secondary.c
@@ -5,24 +5,50 @@
  */
 
 #include <cv_heap_secondary.h>
+#include <cv_debug.h>
+#include <cv_sizeof.h>
 
 static long const g_heap_secondary_grow_len = 4096L;
+
+/*
+ *
+ */
 
 cv_bool cv_heap_secondary_init( cv_heap_secondary * p_this) {
     cv_bool b_result = cv_false;
     cv_heap_section_desc o_desc = cv_heap_section_desc_initializer_;
+    cv_debug_assert_(!!p_this, cv_debug_code_null_ptr);
+    cv_debug_init_(p_this, cv_sizeof_(*p_this));
     o_desc.i_grow_len = g_heap_secondary_grow_len;
     if (cv_heap_section_lock_init(&p_this->o_heap_section_lock,
             &o_desc)) {
         b_result = cv_true;
+    } else {
+        cv_debug_cleanup_(p_this, cv_sizeof_(*p_this));
     }
     return b_result;
 }
 
+/*
+ *
+ */
+
 void cv_heap_secondary_cleanup( cv_heap_secondary * p_this) {
+    cv_debug_assert_(!!p_this, cv_debug_code_null_ptr);
     cv_heap_section_lock_cleanup(&p_this->o_heap_section_lock);
+    cv_debug_cleanup_(p_this, cv_sizeof_(*p_this));
 }
 
+/*
+ *
+ */
+
 void * cv_heap_secondary_alloc( cv_heap_secondary * p_this, long i_len) {
-    return cv_heap_section_lock_alloc(&p_this->o_heap_section_lock, i_len);
+    void * p_result = cv_null_;
+    cv_debug_assert_(!!p_this, cv_debug_code_null_ptr);
+    p_result = cv_heap_section_lock_alloc(&p_this->o_heap_section_lock,
+        i_len);
+    return p_result;
 }
+
+/* end-of-file: cv_heap_secondary.c */
