74bc122cd4d87c011424a661e7c43cd96c6acc03
Felix Boucher <fboucher9@gmail.com>
2019-12-16 06:33:56 -0500

    use stdout for trace and recursion protect report

--- cv_trace/cv_trace_node.c
+++ cv_trace/cv_trace_node.c
@@ -13,11 +13,11 @@
 #include <cv_clock/cv_clock_mono.h>
 #include <cv_clock/cv_clock_tool.h>
 #include <cv_debug/cv_debug.h>
-#include <cv_algo/cv_array_it.h>
-#include <cv_algo/cv_array_print.h>
 #include <cv_number_desc.h>
 #include <cv_thread/cv_mutex.h>
 #include <cv_misc/cv_thread_local.h>
+#include <cv_file/cv_file_std.h>
+#include <cv_file/cv_file_print.h>
 
 static cv_trace_node g_trace_footer =
 cv_trace_node_initializer_(cv_trace_level_0, "footer");
@@ -25,6 +25,8 @@ cv_trace_node_initializer_(cv_trace_level_0, "footer");
 static cv_trace_node * g_trace_list = &g_trace_footer;
 
 static cv_mutex g_trace_mutex = cv_mutex_initializer_;
+
+static cv_thread_local_ long i_recursive = 0;
 
 /*
  *
@@ -44,19 +46,16 @@ static void cv_trace_node_register( cv_trace_node * p_trace_node ) {
 
 void cv_trace_node_dispatch( cv_trace_node * p_trace_node,
     unsigned char i_type) {
-    static cv_thread_local_ long i_recursive = 0;
     if (0 == (i_recursive++)) {
         /* mutex to protect list */
         /* link node into global list */
         /* read current clock */
         /* print of type */
-        unsigned char a_line[80u];
-        cv_array_it o_array_it = cv_array_it_initializer_;
+        cv_file const * p_stdout = cv_file_std_out();
         cv_debug_assert_(p_trace_node, cv_debug_code_null_ptr);
         cv_mutex_impl_lock(&g_trace_mutex);
         cv_trace_node_register(p_trace_node);
         cv_mutex_impl_unlock(&g_trace_mutex);
-        cv_array_it_init_range(&o_array_it, a_line, a_line+sizeof(a_line));
         /* Update stats */
         /* process enter */
         /* process leave */
@@ -66,28 +65,19 @@ void cv_trace_node_dispatch( cv_trace_node * p_trace_node,
             if (cv_clock_mono_read(&o_value)) {
                 cv_clock_msec o_value_msec = cv_clock_msec_initializer_;
                 cv_clock_get_msec(&o_value.o_clock, &o_value_msec);
-                cv_array_print_unsigned(&o_array_it,
+                cv_file_print_unsigned(p_stdout,
                     o_value_msec.i_seconds, cv_number_format_dec());
-                cv_array_print_char(&o_array_it, '.');
-                cv_array_print_unsigned(&o_array_it,
+                cv_file_print_char(p_stdout, '.');
+                cv_file_print_unsigned(p_stdout,
                     o_value_msec.i_mseconds, cv_number_format_dec());
-                cv_array_print_char(&o_array_it, ':');
+                cv_file_print_char(p_stdout, ':');
             }
         }
-        cv_array_print_unsigned(&o_array_it, i_type, cv_number_format_dec());
-        cv_array_print_char(&o_array_it, ':');
-        cv_array_print_0(&o_array_it,
+        cv_file_print_unsigned(p_stdout, i_type, cv_number_format_dec());
+        cv_file_print_char(p_stdout, ':');
+        cv_file_print_0(p_stdout,
             p_trace_node->pc_text, cv_signed_short_max_);
-        if (cv_array_print_nl(&o_array_it)) {
-            if ((o_array_it.o_array.o_min.pc_uchar >= a_line) &&
-                (o_array_it.o_array.o_min.pc_uchar <= a_line+sizeof(a_line))) {
-                long const i_line_len = (o_array_it.o_array.o_min.pc_uchar -
-                    a_line) & cv_signed_long_max_;
-                int const i_stdout = cv_runtime_stdout_fileno();
-                cv_runtime_write(i_stdout, a_line, i_line_len);
-            }
-        }
-        cv_array_it_cleanup(&o_array_it);
+        cv_file_print_nl(p_stdout);
     }
     i_recursive --;
 }
@@ -98,21 +88,10 @@ void cv_trace_node_dispatch( cv_trace_node * p_trace_node,
 
 static void cv_trace_node_profile_report_cb(
     cv_trace_node * p_iterator) {
-    unsigned char a_line[80u];
-    cv_array_it o_array_it = cv_array_it_initializer_;
-    cv_array_it_init_range(&o_array_it, a_line, a_line+sizeof(a_line));
-    cv_array_print_0(&o_array_it, p_iterator->pc_text,
+    cv_file const * p_stdout = cv_file_std_out();
+    cv_file_print_0(p_stdout, p_iterator->pc_text,
         cv_signed_short_max_);
-    if (cv_array_print_nl(&o_array_it)) {
-        if ((o_array_it.o_array.o_min.pc_uchar >= a_line) &&
-            (o_array_it.o_array.o_min.pc_uchar <= a_line+sizeof(a_line))) {
-            long const i_line_len = (o_array_it.o_array.o_min.pc_uchar -
-                a_line) & cv_signed_long_max_;
-            int const i_stdout = cv_runtime_stdout_fileno();
-            cv_runtime_write(i_stdout, a_line, i_line_len);
-        }
-    }
-    cv_array_it_cleanup(&o_array_it);
+    cv_file_print_nl(p_stdout);
 }
 
 /*
@@ -120,11 +99,14 @@ static void cv_trace_node_profile_report_cb(
  */
 
 void cv_trace_node_profile_report(void) {
-    cv_trace_node * p_iterator = g_trace_list;
-    while (p_iterator && (p_iterator != &g_trace_footer)) {
-        cv_trace_node_profile_report_cb(p_iterator);
-        p_iterator = p_iterator->p_next;
+    if (0 == (i_recursive++)) {
+        cv_trace_node * p_iterator = g_trace_list;
+        while (p_iterator && (p_iterator != &g_trace_footer)) {
+            cv_trace_node_profile_report_cb(p_iterator);
+            p_iterator = p_iterator->p_next;
+        }
     }
+    i_recursive --;
 }
 
 /* end-of-file: cv_trace_node.c */
