e74708546f5e05aa282f0920b55d29ad813bb0bf
Felix Boucher <fboucher9@gmail.com>
2020-10-17 08:36:00 -0400

    test of utf16be encoder

--- cv_unicode/cv_unicode_test.c
+++ cv_unicode/cv_unicode_test.c
@@ -7,11 +7,60 @@
 #include <cv_unicode/cv_unicode_test.h>
 #include <cv_unicode/cv_utf16.h>
 #include <cv_unicode/cv_utf16be_decoder.h>
+#include <cv_unicode/cv_utf16be_encoder.h>
 #include <cv_unicode/cv_utf8_decoder.h>
 #include <cv_unicode/cv_utf8_encoder.h>
 #include <cv_algo/cv_array.h>
 #include <cv_algo/cv_array_it.h>
 #include <cv_test_print.h>
+
+static void step_utf16be_encoder(
+    cv_utf16be_encoder * p_this,
+    unsigned long i_input) {
+    cv_uptr i_count = cv_utf16be_encoder_produce(p_this, i_input);
+    if (i_count) {
+        cv_uptr i_index = 0;
+        while (i_index < i_count) {
+            unsigned char i_output = 0;
+            if (cv_utf16be_encoder_consume(p_this, &i_output)) {
+                if (i_output) {
+                    cv_print_0("0x", 80);
+                    cv_print_hex(i_output);
+                } else {
+                    cv_print_0("0x00", 80);
+                }
+                if ((2 == i_count) && (0x10000ul > i_input)) {
+                    if (0 == i_index) {
+                        if ((0xd8 <= i_output) && (0xdf >= i_output)) {
+                            cv_print_char('!');
+                        }
+                    }
+                } else if ((4 == i_count) && (0x10000ul <= i_input)) {
+                    if (0 == i_index) {
+                        if ((0xd8 <= i_output) && (0xdb >= i_output)) {
+                        } else {
+                            cv_print_char('!');
+                        }
+                    } else if (2 == i_index) {
+                        if ((0xdc <= i_output) && (0xdf >= i_output)) {
+                        } else {
+                            cv_print_char('!');
+                        }
+                    }
+                } else {
+                    cv_print_char('!');
+                }
+            } else {
+                cv_print_0("0x??", 80);
+            }
+            i_index ++;
+            if (i_index < i_count) {
+                cv_print_0(", ", 80);
+            }
+        }
+        cv_print_nl();
+    }
+}
 
 static void step_utf16be_decoder(
     cv_utf16be_decoder * p_this,
@@ -53,27 +102,40 @@ static void step_utf8_encoder(
     cv_uptr i_count = cv_utf8_encoder_produce(p_this, i_input);
     if (i_count) {
         cv_uptr i_index = 0;
-        while (i_count --) {
+        while (i_index < i_count) {
             unsigned char i_output = 0;
-            cv_utf8_encoder_consume(p_this, &i_output);
-            cv_print_0("0x", 80);
-            cv_print_hex(i_output);
-            if (0 == i_index) {
-                if ((0xc0 <= i_output) && (0xf7 >= i_output)) {
+            if (cv_utf8_encoder_consume(p_this, &i_output)) {
+                if (i_output) {
+                    cv_print_0("0x", 80);
+                    cv_print_hex(i_output);
                 } else {
-                    cv_print_char('!');
+                    cv_print_0("0x00", 80);
+                }
+                if (1 == i_count) {
+                    if (0x7f >= i_output) {
+                    } else {
+                        cv_print_char('!');
+                    }
+                } else {
+                    if (0 == i_index) {
+                        if ((0xc0 <= i_output) && (0xf7 >= i_output)) {
+                        } else {
+                            cv_print_char('!');
+                        }
+                    } else {
+                        if ((0x80 <= i_output) && (0xc0 > i_output)) {
+                        } else {
+                            cv_print_char('!');
+                        }
+                    }
                 }
             } else {
-                if ((0x80 <= i_output) && (0xc0 > i_output)) {
-                } else {
-                    cv_print_char('!');
-                }
+                cv_print_0("0x??", 80);
             }
-            if (i_count) {
-                cv_print_char(',');
-            }
-            cv_print_char(' ');
             i_index ++;
+            if (i_index < i_count) {
+                cv_print_0(", ", 80);
+            }
         }
         cv_print_nl();
     }
@@ -122,6 +184,21 @@ void cv_unicode_test(void) {
         cv_array_cleanup(&o_storage);
     }
 
+    cv_print_0("utf16be encoder", 80);
+    cv_print_nl();
+    {
+        cv_utf16be_encoder o_encoder;
+        cv_utf16be_encoder_init(&o_encoder);
+        step_utf16be_encoder(&o_encoder, 0x41);
+        step_utf16be_encoder(&o_encoder, 0x1234);
+        step_utf16be_encoder(&o_encoder, 0xffff);
+        step_utf16be_encoder(&o_encoder, 0x10000);
+        step_utf16be_encoder(&o_encoder, 0x10ffff);
+        cv_utf16be_encoder_cleanup(&o_encoder);
+    }
+
+    cv_print_0("utf16be decoder", 80);
+    cv_print_nl();
     {
         cv_utf16be_decoder o_decoder;
         cv_utf16be_decoder_init(&o_decoder);
@@ -138,9 +215,23 @@ void cv_unicode_test(void) {
         cv_utf16be_decoder_cleanup(&o_decoder);
     }
 
+    cv_print_0("utf8 encoder", 80);
+    cv_print_nl();
+    {
+        cv_utf8_encoder o_encoder;
+        cv_utf8_encoder_init(&o_encoder);
+        step_utf8_encoder(&o_encoder, 0x41);
+        step_utf8_encoder(&o_encoder, 0x1234);
+        step_utf8_encoder(&o_encoder, 0x10000);
+        step_utf8_encoder(&o_encoder, 0x10ffff);
+        cv_utf8_encoder_cleanup(&o_encoder);
+    }
+    cv_print_0("utf8 decoder", 80);
+    cv_print_nl();
     {
         cv_utf8_decoder o_decoder;
         cv_utf8_decoder_init(&o_decoder);
+        step_utf8_decoder(&o_decoder, 0x41);
         step_utf8_decoder(&o_decoder, 0xe1);
         step_utf8_decoder(&o_decoder, 0x88);
         step_utf8_decoder(&o_decoder, 0xb4);
@@ -149,15 +240,6 @@ void cv_unicode_test(void) {
         step_utf8_decoder(&o_decoder, 0xbf);
         step_utf8_decoder(&o_decoder, 0xbf);
         cv_utf8_decoder_cleanup(&o_decoder);
-    }
-    {
-        cv_utf8_encoder o_encoder;
-        cv_utf8_encoder_init(&o_encoder);
-        step_utf8_encoder(&o_encoder, 0x1234);
-        step_utf8_encoder(&o_encoder, 0x10000);
-        step_utf8_encoder(&o_encoder, 0x10ffff);
-        step_utf8_encoder(&o_encoder, 0x1fffff);
-        cv_utf8_encoder_cleanup(&o_encoder);
     }
 }
 
