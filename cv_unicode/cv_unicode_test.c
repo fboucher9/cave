ecd3d88fb08fb00223404af58031d68f22d068dd
Felix Boucher <fboucher9@gmail.com>
2020-10-17 07:56:05 -0400

    test utf8 encoder and fix bugs

--- cv_unicode/cv_unicode_test.c
+++ cv_unicode/cv_unicode_test.c
@@ -8,9 +8,76 @@
 #include <cv_unicode/cv_utf16.h>
 #include <cv_unicode/cv_utf16be_decoder.h>
 #include <cv_unicode/cv_utf8_decoder.h>
+#include <cv_unicode/cv_utf8_encoder.h>
 #include <cv_algo/cv_array.h>
 #include <cv_algo/cv_array_it.h>
 #include <cv_test_print.h>
+
+static void step_utf16be_decoder(
+    cv_utf16be_decoder * p_this,
+    unsigned char i_input) {
+    if (cv_utf16be_decoder_produce(p_this, i_input)) {
+        unsigned long i_output = 0;
+        if (cv_utf16be_decoder_consume(p_this, &i_output)) {
+            cv_print_0("0x", 80);
+            cv_print_hex(i_output);
+            if (i_output <= 0x10fffful) {
+            } else {
+                cv_print_char('!');
+            }
+            cv_print_nl();
+        }
+    }
+}
+
+static void step_utf8_decoder(
+    cv_utf8_decoder * p_this,
+    unsigned char i_input) {
+    if (cv_utf8_decoder_produce(p_this, i_input)) {
+        unsigned long i_output = 0;
+        if (cv_utf8_decoder_consume(p_this, &i_output)) {
+            cv_print_0("0x", 80);
+            cv_print_hex(i_output);
+            if (i_output <= 0x10fffful) {
+            } else {
+                cv_print_char('!');
+            }
+            cv_print_nl();
+        }
+    }
+}
+
+static void step_utf8_encoder(
+    cv_utf8_encoder * p_this,
+    unsigned long i_input) {
+    cv_uptr i_count = cv_utf8_encoder_produce(p_this, i_input);
+    if (i_count) {
+        cv_uptr i_index = 0;
+        while (i_count --) {
+            unsigned char i_output = 0;
+            cv_utf8_encoder_consume(p_this, &i_output);
+            cv_print_0("0x", 80);
+            cv_print_hex(i_output);
+            if (0 == i_index) {
+                if ((0xc0 <= i_output) && (0xf7 >= i_output)) {
+                } else {
+                    cv_print_char('!');
+                }
+            } else {
+                if ((0x80 <= i_output) && (0xc0 > i_output)) {
+                } else {
+                    cv_print_char('!');
+                }
+            }
+            if (i_count) {
+                cv_print_char(',');
+            }
+            cv_print_char(' ');
+            i_index ++;
+        }
+        cv_print_nl();
+    }
+}
 
 /*
  *
@@ -58,52 +125,39 @@ void cv_unicode_test(void) {
     {
         cv_utf16be_decoder o_decoder;
         cv_utf16be_decoder_init(&o_decoder);
-        {
-            unsigned long i_output = 0;
-            cv_utf16be_decoder_produce(&o_decoder, 0x12);
-            cv_utf16be_decoder_produce(&o_decoder, 0x34);
-            cv_utf16be_decoder_consume(&o_decoder, &i_output);
-            cv_print_0("0x", 80);
-            cv_print_hex(i_output);
-            cv_print_nl();
-            cv_utf16be_decoder_produce(&o_decoder, 0xd8);
-            cv_utf16be_decoder_produce(&o_decoder, 0x00);
-            cv_utf16be_decoder_produce(&o_decoder, 0xdc);
-            cv_utf16be_decoder_produce(&o_decoder, 0x00);
-            cv_utf16be_decoder_consume(&o_decoder, &i_output);
-            cv_print_0("0x", 80);
-            cv_print_hex(i_output);
-            cv_print_nl();
-            cv_utf16be_decoder_produce(&o_decoder, 0xdb);
-            cv_utf16be_decoder_produce(&o_decoder, 0xff);
-            cv_utf16be_decoder_produce(&o_decoder, 0xdf);
-            cv_utf16be_decoder_produce(&o_decoder, 0xff);
-            cv_utf16be_decoder_consume(&o_decoder, &i_output);
-            cv_print_0("0x", 80);
-            cv_print_hex(i_output);
-            cv_print_nl();
-        }
+        step_utf16be_decoder(&o_decoder, 0x12);
+        step_utf16be_decoder(&o_decoder, 0x34);
+        step_utf16be_decoder(&o_decoder, 0xd8);
+        step_utf16be_decoder(&o_decoder, 0x00);
+        step_utf16be_decoder(&o_decoder, 0xdc);
+        step_utf16be_decoder(&o_decoder, 0x00);
+        step_utf16be_decoder(&o_decoder, 0xdb);
+        step_utf16be_decoder(&o_decoder, 0xff);
+        step_utf16be_decoder(&o_decoder, 0xdf);
+        step_utf16be_decoder(&o_decoder, 0xff);
         cv_utf16be_decoder_cleanup(&o_decoder);
     }
 
     {
-        unsigned long i_output = 0;
         cv_utf8_decoder o_decoder;
         cv_utf8_decoder_init(&o_decoder);
-        cv_utf8_decoder_produce(&o_decoder, 0x41);
-        cv_utf8_decoder_consume(&o_decoder, &i_output);
-        cv_print_0("0x", 80);
-        cv_print_hex(i_output);
-        cv_print_nl();
-        cv_utf8_decoder_produce(&o_decoder, 0xf4);
-        cv_utf8_decoder_produce(&o_decoder, 0x8f);
-        cv_utf8_decoder_produce(&o_decoder, 0xbf);
-        cv_utf8_decoder_produce(&o_decoder, 0xbf);
-        cv_utf8_decoder_consume(&o_decoder, &i_output);
-        cv_print_0("0x", 80);
-        cv_print_hex(i_output);
-        cv_print_nl();
+        step_utf8_decoder(&o_decoder, 0xe1);
+        step_utf8_decoder(&o_decoder, 0x88);
+        step_utf8_decoder(&o_decoder, 0xb4);
+        step_utf8_decoder(&o_decoder, 0xf4);
+        step_utf8_decoder(&o_decoder, 0x8f);
+        step_utf8_decoder(&o_decoder, 0xbf);
+        step_utf8_decoder(&o_decoder, 0xbf);
         cv_utf8_decoder_cleanup(&o_decoder);
+    }
+    {
+        cv_utf8_encoder o_encoder;
+        cv_utf8_encoder_init(&o_encoder);
+        step_utf8_encoder(&o_encoder, 0x1234);
+        step_utf8_encoder(&o_encoder, 0x10000);
+        step_utf8_encoder(&o_encoder, 0x10ffff);
+        step_utf8_encoder(&o_encoder, 0x1fffff);
+        cv_utf8_encoder_cleanup(&o_encoder);
     }
 }
 
--- cv_unicode/cv_utf8_encoder.c
+++ cv_unicode/cv_utf8_encoder.c
@@ -40,24 +40,27 @@ cv_uptr cv_utf8_encoder_produce(cv_utf8_encoder * p_this,
         p_this->i_count = 1;
     } else if (0x7fful >= i_input) {
         unsigned long const l_temp = ((i_input >> 6u) & 0x1ful) | 0xc0ul;
+        unsigned long const l_temp2 = ((i_input >> 0u) & 0x3ful) | 0x80ul;
         p_this->a_accum[1u] = l_temp & 0xff;
-        p_this->a_accum[0u] = i_input & 0x3f;
+        p_this->a_accum[0u] = l_temp2 & 0xff;
         p_this->i_count = 2;
     } else if (0xfffful >= i_input) {
         unsigned long const l_temp1 = ((i_input >> 12u) & 0x0ful) | 0xe0ul;
-        unsigned long const l_temp2 = (i_input >> 6u) & 0x3ful;
+        unsigned long const l_temp2 = ((i_input >> 6u) & 0x3ful) | 0x80ul;
+        unsigned long const l_temp3 = ((i_input >> 0u) & 0x3ful) | 0x80ul;
         p_this->a_accum[2u] = l_temp1 & 0xff;
         p_this->a_accum[1u] = l_temp2 & 0xff;
-        p_this->a_accum[0u] = i_input & 0x3f;
+        p_this->a_accum[0u] = l_temp3 & 0xff;
         p_this->i_count = 3;
     } else if (0x10fffful >= i_input) {
-        unsigned long const l_temp1 = ((i_input >> 18u) & 0x0ful) | 0xe0ul;
-        unsigned long const l_temp2 = (i_input >> 12u) & 0x3ful;
-        unsigned long const l_temp3 = (i_input >> 6u) & 0x3ful;
+        unsigned long const l_temp1 = ((i_input >> 18u) & 0x07ul) | 0xf0ul;
+        unsigned long const l_temp2 = ((i_input >> 12u) & 0x3ful) | 0x80ul;
+        unsigned long const l_temp3 = ((i_input >> 6u) & 0x3ful) | 0x80ul;
+        unsigned long const l_temp4 = ((i_input >> 0u) & 0x3ful) | 0x80ul;
         p_this->a_accum[3u] = l_temp1 & 0xff;
         p_this->a_accum[2u] = l_temp2 & 0xff;
         p_this->a_accum[1u] = l_temp3 & 0xff;
-        p_this->a_accum[0u] = i_input & 0x3f;
+        p_this->a_accum[0u] = l_temp4 & 0xff;
         p_this->i_count = 4;
     }
     return p_this->i_count;
